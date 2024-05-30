735c9a53f87c2aec1aa3d7928c572d94f78ccc67
Looney <62479506+luka-nedeljkov@users.noreply.github.com>
2024-05-30 02:36:36 +0200

    Update install-packages.sh

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -3,7 +3,13 @@
 set -e
 
 # Programs
-pacman -S --noconfirm alacritty bat curl dosfstools e2fsprogs firefox gimp git gptfdisk jdk17-openjdk jdk8-openjdk libreoffice-still man-db man-pages neovim openssh pacman-contrib smartmontools texinfo tmux tree wget
+pacman -S --noconfirm bashtop bat curl git jdk21-openjdk jdk17-openjdk jdk8-openjdk lf man-db man-pages neovim nvtop pacman-contrib tmux tree zoxide
+
+# Apps
+pacman -S --noconfirm alacritty firefox gimp gnome-calculator gwenview libreoffice-fresh obs-studio okular qbittorrent
 
 # GPU Driver
 pacman -S --noconfirm mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau
+
+# Games
+pacman -S --noconfirm lutris steam flatpak

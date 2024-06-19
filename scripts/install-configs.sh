4ea94c4ef52cd0cf15b9353d9c175f0bb2074b14
Luka Nedeljkov <luka.nedeljkov@gmail.com>
2024-06-19 18:10:20 +0200

    Added screenshoting and lf configs

--- scripts/install-configs.sh
+++ scripts/install-configs.sh
@@ -2,16 +2,16 @@
 
 # Alacritty
 mkdir -p ~/.config/alacritty/themes
-cp ../.config/alacritty/themes/catppuccin_mocha.toml ~/.config/alacritty/themes/catppuccin_mocha.toml
-cp ../.config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
+cp ../config/alacritty/themes/catppuccin_mocha.toml ~/.config/alacritty/themes/catppuccin_mocha.toml
+cp ../config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
 
 # bash
-cp ../.bashrc ~/.bashrc
+cp ../home/.bashrc ~/.bashrc
 
 # git
-cp ../.gitconfig ~/.gitconfig
+cp ../home/.gitconfig ~/.gitconfig
 
 # tmux
 mkdir ~/.config/tmux
-cp ../.config/tmux/tmux.conf ~/.config/tmux/tmux.conf
+cp ../config/tmux/tmux.conf ~/.config/tmux/tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
